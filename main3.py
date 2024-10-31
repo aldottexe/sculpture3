0507adb667c97d398193d637abf144f593dae9c3
aldottexe <aldottexe@gmail.com>
2024-10-31 19:51:33 -0400

    added new version for servo control

---
+++ main3.py
@@ -0,0 +1,38 @@
+import torch
+import numpy as np
+from torchvision import models, transforms
+import RPi.GPIO as GPIO
+
+import cv2
+from PIL import Image
+
+cap = cv2.VideoCapture(0)
+    
+pin = 14
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(pin, GPIO.OUT)
+
+model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
+
+with torch.no_grad():
+    while True:
+        # read frame
+        ret, image = cap.read()
+        if not ret:
+            raise RuntimeError("failed to read frame")
+
+        # convert opencv output from BGR to RGB
+        image = image[:, :, [2, 1, 0]]
+
+        # run model
+        output = model(image)
+        # do something with output ...
+        print('total', output.xywhn[0].numpy())
+        people = [p for p in output.xywhn[0].numpy() if ((float(p[-1]) == 0) & (float(p[-2]) > 0.6))]
+        if len(people) > 0:
+            print('fire!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+            GPIO.output(pin, GPIO.HIGH)
+        else:
+            GPIO.output(pin, GPIO.LOW)
+
+        print(output)

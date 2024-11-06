1531d10143d552b8b4ce3501a9faa2be6be92566
aldottexe <aldottexe@gmail.com>
2024-11-06 15:45:38 -0500

    removed old main, and renamed main2

--- main.py
+++ main.py
@@ -1,31 +1,18 @@
-import time
-
 import torch
 import numpy as np
 from torchvision import models, transforms
+import RPi.GPIO as GPIO
 
 import cv2
 from PIL import Image
 
-torch.backends.quantized.engine = 'qnnpack'
+cap = cv2.VideoCapture(0)
+    
+pin = 14
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(pin, GPIO.OUT)
 
-cap = cv2.VideoCapture(0, cv2.CAP_V4L2)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 224)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 224)
-cap.set(cv2.CAP_PROP_FPS, 36)
-
-preprocess = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-])
-
-net = models.quantization.mobilenet_v2(pretrained=True, quantize=True)
-# jit model to take it from ~20fps to ~30fps
-net = torch.jit.script(net)
-
-started = time.time()
-last_logged = time.time()
-frame_count = 0
+model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
 
 with torch.no_grad():
     while True:
@@ -36,29 +23,16 @@ with torch.no_grad():
 
         # convert opencv output from BGR to RGB
         image = image[:, :, [2, 1, 0]]
-        permuted = image
-
-        # preprocess
-        input_tensor = preprocess(image)
-
-        # create a mini-batch as expected by the model
-        input_batch = input_tensor.unsqueeze(0)
 
         # run model
-        output = net(input_batch)
+        output = model(image)
         # do something with output ...
+        print('total', output.xywhn[0].numpy())
+        people = [p for p in output.xywhn[0].numpy() if ((float(p[-1]) == 0) & (float(p[-2]) > 0.6))] 
+        if len(people) > 0: 
+            print('fire!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+            GPIO.output(pin, GPIO.HIGH)
+        else:
+            GPIO.output(pin, GPIO.LOW)
 
-        # people = [p for p in output.xywhn[0].numpy() if int(p[-1]) == 0 & int(p[-1]) > 0.6]
-        
-        top = list(enumerate(output[0].softmax(dim=0)))
-        top.sort(key=lambda x: x[1], reverse=True)
-        for idx, val in top[:10]:
-            print(f"{val.item()*100:.2f}% {classes[idx]}")
-
-        # # log model performance
-        # frame_count += 1
-        # now = time.time()
-        # if now - last_logged > 1:
-        #     print(f"{frame_count / (now-last_logged)} fps")
-        #     last_logged = now
-        #     frame_count = 0
+        print(output)
